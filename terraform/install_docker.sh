547c4597d255a4cfb95d76c52c4bc6d470471671
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-08 20:36:56 -0500

    installations

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -4,9 +4,7 @@
 sudo apt update && sudo apt install default-jre
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings>
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
 sudo apt update && sudo apt install jenkins -y
-
 sudo systemctl start jenkins
 
 #INSTALLING TERRAFORM
