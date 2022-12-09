728b12f4a7d29d16a3b6342cfda3b8209669c17b
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-08 20:23:04 -0500

    docker commands

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -20,4 +20,9 @@ sudo apt-get update
 sudo snap install docker 
 sudo snap start docker 
 
+sudo docker-compose build
+sudo docker-compose run app rake db:create db:migrate 
+sleep 3
+sudo docker-compose run app rake db:seed
+sudo docker-compose up
 
