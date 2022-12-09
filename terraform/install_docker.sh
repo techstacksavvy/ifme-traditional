fa1c6dc14aefbb56a0ea781950e2b2792132aa3b
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-08 20:36:04 -0500

    installations

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#INSTALLING JENKINS
 sudo apt update && sudo apt install default-jre
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings>
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -7,12 +8,23 @@ sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenk
 sudo apt update && sudo apt install jenkins -y
 
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 
-sudo passwd jenkins
-sudo su - jenkins -s /bin/bash
+#INSTALLING TERRAFORM
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
 
 
+#DOCKER COMPOSE STEPS
 cd /home/ubuntu
 git clone https://github.com/KuraLabsCohort3-TeamEQ/ifme.git
 cd ifme/
