4b38153d779a8e9c35450b19c7189346aab84bfe
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-10 13:55:24 -0500

    only install docker

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-#INSTALLING JENKINS
-sudo apt -y update && sudo apt -y install default-jre
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-
-#INSTALLING TERRAFORM
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install terraform
-
 #INSTALLING DOCKER 
 cd /home/ubuntu
 git clone https://github.com/kevinsanaycela3/ifme-traditional.git
