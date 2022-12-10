e7e0828c774d9825bb79b5ec434231e2338a4db2
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-10 11:57:34 -0500

    new docker install steps

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -28,6 +28,18 @@ sudo apt-get install terraform
 cd /home/ubuntu
 git clone https://github.com/kevinsanaycela3/ifme-traditional.git
 cd ifme-traditional/ifme/
-sudo apt-get update 
-sudo snap install docker 
-sudo snap start docker 
+
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
