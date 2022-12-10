deaf07e10600344240daefdc507187d6ff4edbd6
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-10 16:44:27 -0500

    change background color

--- terraform/SG.tf
+++ terraform/SG.tf
@@ -26,13 +26,6 @@ resource "aws_security_group" "web_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
 
   }
-   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
 
   egress {
     from_port = 0
