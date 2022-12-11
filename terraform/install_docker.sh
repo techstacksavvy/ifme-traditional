8d334b910bb9c97d3cca7f5f9b2ffb24b56ca4e7
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-10 21:59:23 -0500

    slight edit

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-#INSTALLING DOCKER 
 cd /home/ubuntu
+
+#INSTALLING TERRAFORM
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+#INSTALLING DOCKER 
+
 git clone https://github.com/kevinsanaycela3/ifme-traditional.git
 cd ifme-traditional/ifme/
 
